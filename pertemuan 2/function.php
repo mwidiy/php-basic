c60fcc5036b48692edf3bbc831e91f74edf88303
Widiser <113989888+Widiser@users.noreply.github.com>
2023-08-13 18:12:48 +0700

    menembahkan function coba dan date

---
+++ pertemuan 2/function.php
@@ -0,0 +1,9 @@
+<?php
+    function coba (){
+        echo "tes tes";
+    };
+    // date = menampilkan tanggal dengan parameters l = nama hari , d = tanggal , M = bulan , Y = tahun
+    echo date("l,d-M-Y");
+
+
+?>
